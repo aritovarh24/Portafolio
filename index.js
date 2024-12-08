6c377b25ede5b7d6baa4996a672909ea64405047
aritovarh24 <aritovarh2404@gmail.com>
2024-12-08 00:42:30 -0500

    Portafolio terminado
    Portafolio de Ariana Tovar Terminado

---
+++ index.js
@@ -0,0 +1,25 @@
+const ux = document.querySelector("#ux");
+const branding = document.querySelector("#branding");
+const photography = document.querySelector("#photography");
+const ilustration = document.querySelector("#Ilustration");
+const editorial = document.querySelector("#editoriall");
+
+ux.addEventListener('click', () => {
+    window.location.href = "html/UX.html";
+})
+
+branding.addEventListener('click', () => {
+    window.location.href = "html/branding.html";
+})
+
+photography.addEventListener('click', () => {
+    window.location.href = "html/photography.html";
+})
+
+ilustration.addEventListener('click', () => {
+    window.location.href = "html/Ilustration.html";
+})
+
+editorial.addEventListener('click', () => {
+    window.location.href = "html/editorial.html";
+})
